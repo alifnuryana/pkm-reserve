1d67dfa464d5cf59aee8721770a644963be2b568
Alif Nuryana <alifnuryana0@gmail.com>
2023-03-08 19:27:18 +0700

    :package: install AlpineJs and alpine-typewritter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+import '../css/app.css';
+import Alpine from 'alpinejs'
+import Typewriter from '@marcreichel/alpine-typewriter';
+
+window.Alpine = Alpine
+
+Alpine.plugin(Typewriter);
+Alpine.start()
